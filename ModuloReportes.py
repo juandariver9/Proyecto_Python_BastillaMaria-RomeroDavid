aff128d364be5e56a5d6a9ce443c77832411c1dd
MariA <mbastilla2007@gmai.com>
2024-02-12 13:36:08 -0500

    mmmajo

--- ModuloReportes.py
+++ ModuloReportes.py
@@ -4,9 +4,13 @@ def Listainscritos ():
     mijson= json.load (x)
     print (" Por favor de enter para mostrar Campers en estado inscrito")
     enter=input("")
-    print(mijson)
-
+    listains=mijson[ "datos"]["inscripciones"]
+    for i in listains:
+        for llave,valor in i.items():
+             print (f"{llave}: {valor}")
+        print("-------------------------\n")
 Listainscritos()
+
 
 
 
@@ -24,6 +28,21 @@ def ListaAprobaronExamen():
 
     for camper in listaaprobaron:
         for llave, valor in camper.items():
+            print (f"{llave}: {valor}")
+        print("-------------------------\n")
+
+
+def listatrainerscampus():
+    import json
+    x=open("datos.json", 'r')
+    data = json.load(x)
+
+    listatrainers= data["Datos"]["Trainer_Principales"]
+    print("Por favor, presiona Enter si deseas visualizar los campers que aprobaron el examen inicial")
+    enter = input("")
+
+    for trainers in listatrainers:
+        for llave, valor in trainers.items():
             print (f"{llave}: {valor}")
         print("-------------------------\n")
 
