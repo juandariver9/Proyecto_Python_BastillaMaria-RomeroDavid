cf8c565c98c23d4dc6207fabdf89feb8442f80d5
Majob2027 <mbastilla2007@gmail.com>
2024-02-12 16:07:17 -0500

    kkk

---
+++ Modulonotas.py
@@ -0,0 +1,10 @@
+# Modulo para agregar notas 
+
+print (" Que modulo desea actulizar")
+
+print ( "1.  fundamentos de la programacion  \t 2. Programación Web ")
+print(" ")
+print("3. Programación formal\t 4. Bases de datos")
+print ("")
+print("4. Backend")
+decision = int(input("--->"))
