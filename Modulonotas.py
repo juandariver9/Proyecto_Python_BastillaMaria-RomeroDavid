88ab38c3aaf9de1be808f67a2643581430f0b926
Maria <mbastilla2007@gmail.com>
2024-02-13 07:03:37 -0500

    majo

--- Modulonotas.py
+++ Modulonotas.py
@@ -1,15 +1,12 @@
 # Modulo para agregar notas 
 def fundamentosprogramacion():
     def notafinal ():
-       notaf= pruebapractica*0.6 + pruebateorica*0.6
-       notat= 
+       notapruebas= pruebapractica*0.6 + pruebateorica*0.3
+       notatalleres= (totalquizes+totaltrabajos)*0.1
+       notafinal= notapruebas+notatalleres
+       return notafinal
+
        
-
-
-
-
-
-
     from ModuloReportes import ListaAprobaronExamen
     print ("Por favor escriba el id del estudiante a actualizar")
     idestudiante=int(input("---->"))
@@ -19,14 +16,25 @@ def fundamentosprogramacion():
             pruebateorica=int(input("--->"))
             print("Digite nota de prueba practica ")
             pruebapractica=int(input("--->"))
-            print("Digite la antidad de quizes hechos durante el modulo")
+            print("Digite la cantidad de quizes hechos durante el modulo")
             quizes=int(input("--->"))
+            sumatotal=0
             for i in range (quizes):
-             notas=int(input("Digite nota quiz #" (i))
-             print("Digite la antidad de Trabajos hechos durante el modulo")
-             Trabajos=int(input("--->"))
+             notas=int(input("Digite nota quiz #" (i)))
+             sumatotal= sumatotal+notas
+             totalquizes= sumatotal/quizes
+                       
+            print("Digite la antidad de Trabajos hechos durante el modulo")
+            Trabajos=int(input("--->"))
+            sumatotaltrabajos=0
             for i in range (Trabajos):
-             notas=int(input("Digite nota Trabajo #" (i))
+             notas=int(input("Digite nota Trabajo #" (i)))
+             sumatotaltrabajos= sumatotaltrabajos+notas
+             totaltrabajos=sumatotaltrabajos/Trabajos
+    return notafinal
+                       
+
+
     notafinal()
         
 
